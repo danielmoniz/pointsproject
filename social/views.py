7eae571dfd247ff7046aa6aa8d3ec286dcd5df1c
Daniel Moniz <daniel.moniz@gmail.com>
2012-06-04 14:16:47 -0400

    Add list functionality to the new post form

--- social/views.py
+++ social/views.py
@@ -6,29 +6,15 @@ from django.views.generic import CreateView, ListView, DetailView
 
 from social.models import Post, PostForm
 
-def home(request):
-    #return HttpResponse('test')
-    return render_to_response('social/home.html')
-
-class PostListView(ListView):
-    queryset = Post.objects.order_by('-date_created')
-    model = Post
-    template_name = 'social/post_list.html'
-    context_object_name = 'post_list'
-
-    def get_context_data(self, **kwargs):
-        context = super(PostListView, self).get_context_data(**kwargs)
-
-        # Add new_post form to the post list page.
-        context['new_post'] = render_to_response('social/new_post.html')
-
-        return context
-
 class PostCreateView(CreateView):
+    """The view for creating a post. Also handles listing posts."""
     model = Post
     form_class = PostForm
     template_name = 'social/new_post.html'
-    success_url = '/test'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.success_url = reverse('social:new_post')
+        return super(PostCreateView, self).dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         post = form.save(commit=False)
@@ -36,3 +22,17 @@ class PostCreateView(CreateView):
         post.save()
         return HttpResponseRedirect(self.success_url)
 
+    def get_context_data(self, **kwargs):
+        context = super(PostCreateView, self).get_context_data(**kwargs)
+
+        # Add new_post form to the post list page.
+        context['post_list'] = Post.objects.order_by('-date_created')
+
+        return context
+
+### DEPRECATED ###
+class PostListView(ListView):
+    queryset = Post.objects.order_by('-date_created')
+    model = Post
+    template_name = 'social/post_list.html'
+    context_object_name = 'post_list'
